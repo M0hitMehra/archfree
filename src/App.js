1d61fa7b18648a236908ec52fb76cafcb797d915
Mohit Mehra <98013205+M0hitMehra@users.noreply.github.com>
2023-07-27 20:09:33 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+ import './App.css';
+
+function App() {
+  return (
+    <div className={`h-screen w-screen bg-blue-500 flex items-center justify-center font-semibold text-3xl`}>
+     Hello World
+    </div>
+  );
+}
+
+export default App;
