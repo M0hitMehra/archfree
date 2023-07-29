12fac95fd5f28feed2f3f27709fa167ec095e7d0
Mohit Mehra <98013205+M0hitMehra@users.noreply.github.com>
2023-07-29 18:50:00 +0530

    Homepafe#4

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -115,7 +115,7 @@ const Home = () => {
   const sectionAnimation = sectionInView ? "visible" : "hidden";
 
   return (
-    <div className="overflow-x-hidden">
+    <div className=" overflow-x-hidden ">
       {/* main Part with umage */}
       <section id="hero" className="flex items-center justify-center">
         <div className=" ">
@@ -245,16 +245,16 @@ const Home = () => {
 
       {/* SERVICES */}
 
-      <section id="services"  className="services text-white p-24">
-        <div  className="container ">
-          <div  className="section-title mb-12">
-            <h2  className="text-xl font-bold text-slate-300">Services</h2>
+      <section id="services" className="services text-white p-24">
+        <div className="container ">
+          <div className="section-title mb-12">
+            <h2 className="text-xl font-bold text-slate-300">Services</h2>
             <p className="text-4xl font-serif font-bold my-5">
               Check our Services
             </p>
           </div>
           <div className="flex flex-col">
-            <div  className="flex justify-center items-center p-8 gap-14 ">
+            <div className="flex justify-center items-center p-8 gap-14 ">
               {/* Service 1  */}
               <motion.div
                 className="bg-[#ffeeec] flex flex-col text-black justify-evenly items-center gap-2 p-4 rounded-lg grow h-[24rem] w-[4rem] text-center"
@@ -268,16 +268,16 @@ const Home = () => {
                 initial="hidden"
                 ref={sectionRef}
               >
-                <div  className=" h-24 w-24 p-1 flex items-center rounded-lg  justify-center bg-yellow-400 ">
+                <div className=" h-24 w-24 p-1 flex items-center rounded-lg  justify-center bg-yellow-400 ">
                   <img
                     src="	https://www.thearchertechnology.com/public/frontend/img/server.png"
                     alt="Web Development"
-                     className="   "
+                    className="   "
                   />
                 </div>
-                <div  className="flex flex-col gap-4">
-                  <h5  className="  text-2xl font-bold  ">Web Development</h5>
-                  <p  className="text-slate-800">
+                <div className="flex flex-col gap-4">
+                  <h5 className="  text-2xl font-bold  ">Web Development</h5>
+                  <p className="text-slate-800">
                     Your website is the face of your business. The importance of
                     having a business website is to have a strong 24/7 online
                     presence of your business and all are possible at Archer
@@ -299,16 +299,16 @@ const Home = () => {
                 initial="hidden"
                 ref={sectionRef}
               >
-                <div  className=" h-24 w-24 p-1 flex items-center rounded-lg  justify-center bg-yellow-400 ">
+                <div className=" h-24 w-24 p-1 flex items-center rounded-lg  justify-center bg-yellow-400 ">
                   <img
                     src="	https://www.thearchertechnology.com/public/frontend/img/digital-marketing.png"
                     alt="Digital Marketing"
-                     className="   "
+                    className="   "
                   />
                 </div>
-                <div  className="flex flex-col gap-4">
-                  <h5  className=" text-2xl font-bold   ">Digital Marketing</h5>
-                  <p  className="text-slate-800 ">
+                <div className="flex flex-col gap-4">
+                  <h5 className=" text-2xl font-bold   ">Digital Marketing</h5>
+                  <p className="text-slate-800 ">
                     Looking for a Digital Marketing Agency to help you create a
                     strong online presence for your business? With our digital
                     marketing services, you get more bang for your hard-earned
@@ -330,16 +330,16 @@ const Home = () => {
                 initial="hidden"
                 ref={sectionRef}
               >
-                <div  className=" h-24 w-24 p-1 flex rounded-lg  items-center justify-center bg-yellow-400 ">
+                <div className=" h-24 w-24 p-1 flex rounded-lg  items-center justify-center bg-yellow-400 ">
                   <img
                     src="https://www.thearchertechnology.com/public/frontend/img/graphics.png"
                     alt="Graphics Design"
-                     className="   "
+                    className="   "
                   />
                 </div>
-                <div  className="flex flex-col gap-4">
-                  <h5  className="  text-2xl font-bold  ">Graphics Design</h5>
-                  <p  className="text-slate-800 ">
+                <div className="flex flex-col gap-4">
+                  <h5 className="  text-2xl font-bold  ">Graphics Design</h5>
+                  <p className="text-slate-800 ">
                     Our Graphics designer can enhance any website to convert
                     into the beautiful layout, Mobile app interface, Logo Design
                     and many more.
@@ -351,7 +351,7 @@ const Home = () => {
             <div className=" flex gap-24 p-8 justify-center items-center text-black">
               {/* Service  4  */}
               <motion.div
-                 className=" flex  flex-col gap-2 p-4 grow justify-evenly items-center h-[18rem] w-[4rem] text-center bg-[#ffeeec] rounded-lg "
+                className=" flex  flex-col gap-2 p-4 grow justify-evenly items-center h-[18rem] w-[4rem] text-center bg-[#ffeeec] rounded-lg "
                 variants={sections}
                 animate={sectionAnimation} // Use the animation controls
                 whileHover={{
@@ -362,15 +362,17 @@ const Home = () => {
                 initial="hidden"
                 ref={sectionRef}
               >
-                <div  className="h-24 w-24 p-1 bg-yellow-400 flex rounded-lg justify-center items-center ">
+                <div className="h-24 w-24 p-1 bg-yellow-400 flex rounded-lg justify-center items-center ">
                   <img
                     src="	https://www.thearchertechnology.com/public/frontend/img/mobile%20(1).png"
                     alt="Mobile Apps Development"
-                     className=" "
+                    className=" "
                   />
                 </div>
-                <h5  className="   text-2xl font-bold  ">Mobile Apps Development</h5>
-                <p  className="text-slate-800 ">
+                <h5 className="   text-2xl font-bold  ">
+                  Mobile Apps Development
+                </h5>
+                <p className="text-slate-800 ">
                   Every business aims to increase their audience base, brand
                   popularity, sales, and revenue generation. A mobile
                   application is capable of doing all these and helps businesses
@@ -381,7 +383,7 @@ const Home = () => {
 
               {/* Service 5  */}
               <motion.div
-                 className=" flex  flex-col gap-2 p-4 grow justify-evenly items-center h-[18rem] w-[4rem] text-center bg-[#ffeeec] rounded-lg "
+                className=" flex  flex-col gap-2 p-4 grow justify-evenly items-center h-[18rem] w-[4rem] text-center bg-[#ffeeec] rounded-lg "
                 variants={sections}
                 animate={sectionAnimation} // Use the animation controls
                 whileHover={{
@@ -394,15 +396,17 @@ const Home = () => {
                 ref={sectionRef}
               >
                 {" "}
-                <div  className="h-24 w-24 p-1 bg-yellow-400 flex rounded-lg justify-center items-center ">
+                <div className="h-24 w-24 p-1 bg-yellow-400 flex rounded-lg justify-center items-center ">
                   <img
                     src="https://www.thearchertechnology.com/public/frontend/img/software.png"
                     alt="Software Development"
-                     className=" "
+                    className=" "
                   />
                 </div>
-                <h5  className="  text-2xl font-bold   ">Software Development</h5>
-                <p  className="text-center text-slate-800">
+                <h5 className="  text-2xl font-bold   ">
+                  Software Development
+                </h5>
+                <p className="text-center text-slate-800">
                   Software is very important for businesses as it helps them
                   distinguish from competitors and become more competitive.
                   Software developed at Archer Technology can improve the
@@ -418,23 +422,156 @@ const Home = () => {
 
       {/* CTA  */}
 
-      <section id="cta"  className="cta">
-        <div  className="container aos-init aos-animate" data-aos="zoom-in">
-            <div  className="text-center">
-                <h3>why choose us?</h3>
-                <p claa="">
-            
-                Our well crafted Web design and development teams analyze and understand 
-                each project requirement in-depth to bring high performing solutions that 
-                are truly unique to your business challenges and accelerate your business 
-                toward success.
-                </p>
-                <a  className="cta-btn" href="#">Call To Action</a>
-            </div>
+      <section id="cta" className="cta px-24 py-12 ">
+        <div className="text-white">
+          <div className="text-center ">
+            <h3 className="text-4xl font-bold font-[Raleway]">
+              why choose us ?
+            </h3>
+            <p className="my-4 mb-8">
+              Our well crafted Web design and development teams analyze and
+              understand each project requirement in-depth to bring high
+              performing solutions that are truly unique to your business
+              challenges and accelerate your business toward success.
+            </p>
+            <a
+              className="cta-btn font-semibold text-xl border-2 py-2 px-6 border-white rounded-md hover:border-0 hover:text-black hover:bg-[#ffc451]"
+              href="#"
+            >
+              Call To Action
+            </a>
+          </div>
         </div>
-    </section>
+      </section>
 
+      {/* contact us */}
 
+      <section id="contact" className="contact text-white p-24">
+        <div className="container flex flex-col justify-center">
+          <div className="section-title flex flex-col gap-4 mb-4">
+            <h2 className="text-md font-bold text-slate-300 uppercase">
+              Contact
+            </h2>
+            <p className="text-3xl uppercase font-bold">Contact Us</p>
+          </div>
+
+          <div className="">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7419.02598295401!2d71.2269608977129!3d21.6049250781877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395880c5a8ae72db%3A0x3367f8679c1c86eb!2sAngel%20Lords%20Eco%20Inn%20Amreli!5e0!3m2!1sen!2sin!4v1670677249636!5m2!1sen!2sin"
+              width="100%"
+              height="300"
+              style={{ border: "0" }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-5">
+            <div className="info">
+              <div className="address">
+                <i className="bi bi-geo-alt"></i>
+                <h4>Location:</h4>
+                <p>
+                  F-15, 1st Floor, Angel Lords Hotel, Old Marketing Yard,
+                  Amreli, Gujarat, India, 365601
+                </p>
+              </div>
+
+              <div className="email">
+                <i className="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>info@thearchertechnology.com</p>
+              </div>
+
+              <div className="phone">
+                <i className="bi bi-phone"></i>
+                <h4>Call:</h4>
+                <p>+918849993525</p>
+              </div>
+            </div>
+
+            <div>
+              <form
+                id="contactform"
+                role="form"
+                className="php-email-form"
+                noValidate="novalidate"
+              >
+                <input
+                  type="hidden"
+                  name="_token"
+                  value="fY9DJ9Xvstntk2SvF8v8cfhITLZGq7gM3cIL78bS"
+                />
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      name="fullname"
+                      className="form-control error"
+                      id="name"
+                      placeholder="Your Name"
+                      required=""
+                      aria-invalid="true"
+                    />
+                    <label
+                      id="name-error"
+                      className="error text-red-500 text-sm"
+                      htmlFor="name"
+                    >
+                      Name is required
+                    </label>
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      className="form-control valid"
+                      name="email"
+                      id="email"
+                      placeholder="Your Email"
+                      required=""
+                      style={{
+                        backgroundImage: "url('data:image/png;base64,...')",
+                        backgroundRepeat: "no-repeat",
+                        backgroundSize: "20px",
+                        backgroundPosition: "97% center",
+                        cursor: "auto",
+                      }}
+                      aria-invalid="false"
+                    />
+                  </div>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="mobile"
+                    id="mobile"
+                    placeholder="Your Mobile"
+                    required=""
+                  />
+                </div>
+                <div className="form-group">
+                  <textarea
+                    className="form-control"
+                    name="msg"
+                    rows="5"
+                    placeholder="Your Ideas/Message"
+                    required=""
+                  ></textarea>
+                </div>
+
+                <div className="sent-message mt-3" id="notidiv"></div>
+                <div className="text-center mt-4">
+                  <button id="save" className="btn btn-warning">
+                    Send Message
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
