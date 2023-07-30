20588326d247f6fab3356e29b22abed7f5bf7ebe
titusharma12 <sharmatitu119@gmail.com>
2023-07-30 10:41:12 +0530

    -

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -275,7 +275,7 @@ const About = () => {
             </p>
           </motion.div>
         </div>
-        <div className="flex justify-between mt-4 mb-10 w-full">
+        <div className="flex justify-between mt-4  w-full">
           <motion.div className="m-3 w-2/5 h-auto"  variants={sections}
                 animate={sectionAnimation}   initial="hidden"
                 ref={sectionRef}>
