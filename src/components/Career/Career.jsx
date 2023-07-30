907ecc958551d5e1be45199c1b7a2cbaa9b37d0d
titusharma12 <sharmatitu119@gmail.com>
2023-07-30 14:42:06 +0530

    -

--- src/components/Career/Career.jsx
+++ src/components/Career/Career.jsx
@@ -1,9 +1,139 @@
-import React from 'react'
+import React from "react";
 
 const Career = () => {
   return (
-    <div>Career</div>
-  )
-}
+    <div>
+      <section id="career" className="career text-white p-4 md:p-24">
+        <div className="container flex flex-col justify-center gap-6">
+          <div className="section-title flex flex-col gap-4 mb-4">
+            <h2 className="text-md md:text-lg font-bold text-slate-300 uppercase"></h2>
+            <p className="text-xl md:text-3xl uppercase font-bold tracking-widest">
+              CAREER
+            </p>
+          </div>
+          <div className="">
+            <form className="w-full max-w-lg border-2 border-gray rounded p-8">
+              <h3 className="text-2xl font-bold text-white mb-2 ">APPLY NOW</h3>
+              <div className="flex flex-wrap -mx-3 mb-6 mt-4">
+                <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                  <label
+                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    htmlFor="grid-first-name"
+                  >
+                    First Name
+                  </label>
+                  <input
+                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                    id="grid-first-name"
+                    type="text"
+                    placeholder="Jane"
+                  />
+                </div>
+                <div className="w-full md:w-1/2 px-3">
+                  <label
+                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    htmlFor="grid-last-name"
+                  >
+                    Last Name
+                  </label>
+                  <input
+                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-last-name"
+                    type="text"
+                    placeholder="Doe"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                  <label
+                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    htmlFor="grid-password"
+                  >
+                    Email
+                  </label>
+                  <input
+                    className=" block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="email"
+                    type="email"
+                    placeholder="xyz@gmail.com"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                  <label
+                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    htmlFor="grid-password"
+                  >
+                    Mobile
+                  </label>
+                  <input
+                    className=" block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 [&::-webkit-inner-spin-button]:appearance-none"
+                    id="phone_number"
+                    type="number"
+                    placeholder="Phone number"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full  px-3 mb-6 md:mb-0">
+                  <label
+                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    htmlFor="grid-state"
+                  >
+                    Position
+                  </label>
+                  <div className="relative">
+                    <select
+                      className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                      id="grid-state"
+                    >
+                      <option>Select Position</option>
+                      <option>PHP Developer</option>
+                      <option>Business Development Executive</option>
+                    </select>
+                    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                      <svg
+                        className="fill-current h-4 w-4"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                      </svg>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                  <label
+                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    htmlFor="file_input"
+                  >
+                    Resume
+                  </label>
+                  <input
+                    className="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="file_input"
+                    type="file"
+                  />
+                </div>
+              </div>
+              <div className="text-center mt-4 flex justify-end ">
+                <button
+                  id="save"
+                  className="btn bg-yellow-500 hover:bg-yellow-400 px-4 py-3 rounded-md text-black text-xl font-medium"
+                >
+                  Apply Now
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-export default Career
+export default Career;
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,148 +6,149 @@ import { BsPhone } from "react-icons/bs";
 const Contact = () => {
   return (
     <div>
-       <section id="contact" className="contact text-white p-4 md:p-24">
-  <div className="container flex flex-col justify-center gap-6">
-    <div className="section-title flex flex-col gap-4 mb-4">
-      <h2 className="text-md md:text-lg font-bold text-slate-300 uppercase">
-        Contact
-      </h2>
-      <p className="text-xl md:text-3xl uppercase font-bold tracking-widest">
-        Contact Us
-      </p>
-    </div>
-
-    <div className="">
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7419.02598295401!2d71.2269608977129!3d21.6049250781877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395880c5a8ae72db%3A0x3367f8679c1c86eb!2sAngel%20Lords%20Eco%20Inn%20Amreli!5e0!3m2!1sen!2sin!4v1670677249636!5m2!1sen!2sin"
-        width="100%"
-        height="300"
-        style={{ border: "0" }}
-        allowFullScreen=""
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
-    </div>
-
-    <div className="grid grid-cols-1 md:grid-cols-12 gap-6 mt-6 ">
-      <div className="info md:col-span-4 h-auto flex flex-col justify-between gap-7">
-        <div className="address flex items-center gap-4 ">
-          <div className="bg-yellow-400 px-3 py-1 rounded-md mt-[-40px]">
-            <CiLocationOn className="text-black h-8 w-4" />
-          </div>
-          <div>
-            <h4 className="text-lg md:text-2xl font-[raleway] font-medium text-gray-200">
-              Location:
-            </h4>
-            <p className="text-sm md:text-base text-gray-300 mt-2">
-              F-15, 1st Floor, Angel Lords Hotel, Old Marketing Yard, Amreli, Gujarat, India, 365601
+      <section id="contact" className="contact text-white p-4 md:p-24">
+        <div className="container flex flex-col justify-center gap-6">
+          <div className="section-title flex flex-col gap-4 mb-4">
+            <h2 className="text-md md:text-lg font-bold text-slate-300 uppercase">
+              Contact
+            </h2>
+            <p className="text-xl md:text-3xl uppercase font-bold tracking-widest">
+              Contact Us
             </p>
           </div>
-        </div>
 
-        <div className="email flex items-center gap-4 ">
-          <div className="bg-yellow-400 px-3 py-1 rounded-md">
-            <BsPhone className="text-black h-8 w-4" />
+          <div className="">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7419.02598295401!2d71.2269608977129!3d21.6049250781877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395880c5a8ae72db%3A0x3367f8679c1c86eb!2sAngel%20Lords%20Eco%20Inn%20Amreli!5e0!3m2!1sen!2sin!4v1670677249636!5m2!1sen!2sin"
+              width="100%"
+              height="300"
+              style={{ border: "0" }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
           </div>
-          <div>
-            <h4 className="text-lg md:text-2xl font-[raleway] font-medium text-gray-200">
-              Email:
-            </h4>
-            <p className="text-sm md:text-base tracking-widest text-gray-300 mt-2">
-              info@thearchertechnology.com
-            </p>
-          </div>
-        </div>
 
-        <div className="phone flex items-center gap-4 ">
-          <div className="bg-yellow-400 px-3 py-1 rounded-md">
-            <TfiEmail className="text-black h-8 w-4" />
-          </div>
-          <div>
-            <h4 className="text-lg md:text-2xl font-[raleway] font-medium text-gray-200">
-              Call:
-            </h4>
-            <p className="text-sm md:text-base tracking-widest text-gray-300 mt-2">
-              +918849993525
-            </p>
-          </div>
-        </div>
-      </div>
+          <div className="grid grid-cols-1 md:grid-cols-12 gap-6 mt-6 ">
+            <div className="info md:col-span-4 h-auto flex flex-col justify-between gap-7">
+              <div className="address flex items-center gap-4 ">
+                <div className="bg-yellow-400 px-3 py-1 rounded-md mt-[-40px]">
+                  <CiLocationOn className="text-black h-8 w-4" />
+                </div>
+                <div>
+                  <h4 className="text-lg md:text-2xl font-[raleway] font-medium text-gray-200">
+                    Location:
+                  </h4>
+                  <p className="text-sm md:text-base text-gray-300 mt-2">
+                    F-15, 1st Floor, Angel Lords Hotel, Old Marketing Yard,
+                    Amreli, Gujarat, India, 365601
+                  </p>
+                </div>
+              </div>
 
-      <div className="col-span-1 md:col-span-8">
-        <form
-          id="contactform"
-          role="form"
-          className="php-email-form flex flex-col gap-5"
-          noValidate="novalidate"
-        >
-          <div className="flex gap-5">
-            <div className="form-group w-full">
-              <input
-                type="text"
-                name="fullname"
-                className="form-control w-full p-2 rounded-md border-2 border-blue-400 focus:border-bg-400 focus:outline-blue-500 text-sm md:text-base"
-                id="name"
-                placeholder="Your Name"
-                required=""
-                aria-invalid="true"
-              />
+              <div className="email flex items-center gap-4 ">
+                <div className="bg-yellow-400 px-3 py-1 rounded-md">
+                  <BsPhone className="text-black h-8 w-4" />
+                </div>
+                <div>
+                  <h4 className="text-lg md:text-2xl font-[raleway] font-medium text-gray-200">
+                    Email:
+                  </h4>
+                  <p className="text-sm md:text-base tracking-widest text-gray-300 mt-2">
+                    info@thearchertechnology.com
+                  </p>
+                </div>
+              </div>
+
+              <div className="phone flex items-center gap-4 ">
+                <div className="bg-yellow-400 px-3 py-1 rounded-md">
+                  <TfiEmail className="text-black h-8 w-4" />
+                </div>
+                <div>
+                  <h4 className="text-lg md:text-2xl font-[raleway] font-medium text-gray-200">
+                    Call:
+                  </h4>
+                  <p className="text-sm md:text-base tracking-widest text-gray-300 mt-2">
+                    +918849993525
+                  </p>
+                </div>
+              </div>
             </div>
-            <div className="form-group w-full">
-              <input
-                type="email"
-                className="form-control w-full p-2 rounded-md border-2 border-blue-400 focus:border-bg-400 focus:outline-blue-500 text-sm md:text-base"
-                name="email"
-                id="email"
-                placeholder="Your Email"
-                required=""
-                style={{
-                  backgroundImage: "url('data:image/png;base64,...')",
-                  backgroundRepeat: "no-repeat",
-                  backgroundSize: "20px",
-                  backgroundPosition: "97% center",
-                  cursor: "auto",
-                }}
-                aria-invalid="false"
-              />
+
+            <div className="col-span-1 md:col-span-8">
+              <form
+                id="contactform"
+                role="form"
+                className="php-email-form flex flex-col gap-5"
+                noValidate="novalidate"
+              >
+                <div className="flex gap-5">
+                  <div className="form-group w-full">
+                    <input
+                      type="text"
+                      name="fullname"
+                      className="form-control w-full p-2 rounded-md border-2 border-blue-400 focus:border-bg-400 focus:outline-blue-500 text-sm md:text-base"
+                      id="name"
+                      placeholder="Your Name"
+                      required=""
+                      aria-invalid="true"
+                    />
+                  </div>
+                  <div className="form-group w-full">
+                    <input
+                      type="email"
+                      className="form-control w-full p-2 rounded-md border-2 border-blue-400 focus:border-bg-400 focus:outline-blue-500 text-sm md:text-base"
+                      name="email"
+                      id="email"
+                      placeholder="Your Email"
+                      required=""
+                      style={{
+                        backgroundImage: "url('data:image/png;base64,...')",
+                        backgroundRepeat: "no-repeat",
+                        backgroundSize: "20px",
+                        backgroundPosition: "97% center",
+                        cursor: "auto",
+                      }}
+                      aria-invalid="false"
+                    />
+                  </div>
+                </div>
+                <div className="form-group w-full">
+                  <input
+                    type="text"
+                    className="form-control w-full p-2 rounded-md border-2 border-blue-400 focus:border-bg-400 focus:outline-blue-500 text-sm md:text-base"
+                    name="mobile"
+                    id="mobile"
+                    placeholder="Your Mobile"
+                    required=""
+                  />
+                </div>
+                <div className="form-group w-full">
+                  <textarea
+                    className="form-control w-full p-2 rounded-md border-2 border-blue-400 focus:border-bg-400 focus:outline-blue-500 text-sm md:text-base"
+                    name="msg"
+                    rows="5"
+                    placeholder="Your Ideas/Message"
+                    required=""
+                  ></textarea>
+                </div>
+
+                <div className="sent-message mt-3" id="notidiv"></div>
+                <div className="text-center mt-4">
+                  <button
+                    id="save"
+                    className="btn bg-yellow-500 hover:bg-yellow-400 px-3 py-2 rounded-md text-black"
+                  >
+                    Send Message
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
-          <div className="form-group w-full">
-            <input
-              type="text"
-              className="form-control w-full p-2 rounded-md border-2 border-blue-400 focus:border-bg-400 focus:outline-blue-500 text-sm md:text-base"
-              name="mobile"
-              id="mobile"
-              placeholder="Your Mobile"
-              required=""
-            />
-          </div>
-          <div className="form-group w-full">
-            <textarea
-              className="form-control w-full p-2 rounded-md border-2 border-blue-400 focus:border-bg-400 focus:outline-blue-500 text-sm md:text-base"
-              name="msg"
-              rows="5"
-              placeholder="Your Ideas/Message"
-              required=""
-            ></textarea>
-          </div>
-
-          <div className="sent-message mt-3" id="notidiv"></div>
-          <div className="text-center mt-4">
-            <button
-              id="save"
-              className="btn bg-yellow-500 hover:bg-yellow-400 px-3 py-2 rounded-md text-black"
-            >
-              Send Message
-            </button>
-          </div>
-        </form>
-      </div>
+        </div>
+      </section>
     </div>
-  </div>
-</section>
-    </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
